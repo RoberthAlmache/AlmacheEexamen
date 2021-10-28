d0f6a6630b9925cc0b898b346368b48ea7b758e1
Roberth Almache <roberth9501@hotmail.com>
2021-10-28 11:27:14 -0500

    Agregar archivos de proyecto.

---
+++ AlmacheEexamen/Login.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlmacheEexamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Login : ContentPage
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnAbrir_Clicked(object sender, EventArgs e)
+        {
+            //enviar datos como parametros y almacenar en variables
+            string usuario = txtUsuario.Text; //estudiante2021   credenciales quemadas y que permiten el acceso a la siguiente ventana.
+            string contraseña = txtContraseña.Text; //uisrael2021
+
+            if (usuario == "estudiante2021" && contraseña == "uisrael2021")
+            {
+                await Navigation.PushAsync(new Registro(usuario, contraseña));
+
+            }
+            else
+            {
+                await DisplayAlert("Advertencia.!", "Ingrese las credenciales correctas.", "OK");
+            }
+        }
+    }
+}
---
+++ AlmacheEexamen/Registro.xaml.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlmacheEexamen
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Registro : ContentPage
+    {
+        public Registro(string usuario = "estudiante2021", string contraseña = "uisrael2021")
+        {
+            InitializeComponent();
+
+            lblUsuario.Text = usuario;
+
+
+        }
+
+
+
+        private void btnCuotas_Clicked(object sender, EventArgs e)
+        {
+
+            double valor1 = Convert.ToDouble(txtMIni.Text);
+
+            double cuotas1 = 1800 - valor1;
+            double cuotas2 = cuotas1 / 3; // valor de 266,66
+
+            double cuotas3 = 1800 * 0.05; // interes 5%
+            double cuotas4 = cuotas2 + cuotas3;
+            double total = valor1 + cuotas4 * 3;
+
+
+
+            txtCuota1.Text = cuotas4.ToString();
+            txtCuota2.Text = cuotas4.ToString();
+            txtCuota3.Text = cuotas4.ToString();
+            txtTotPag.Text = total.ToString();
+            // txtTotalPagar.Text = cuotas4.ToString();
+        }
+
+        private async void btnGuardar_Clicked(object sender, EventArgs e)
+        {
+            
+            string nombre = txtNEst.Text;
+            string total = txtTotPag.Text;
+
+            //txtTotPag.Text = valor1.ToString();
+            //double valor1 = Convert.ToDouble(txtTotPag.Text);
+
+            await DisplayAlert("Registro Correcto.", "Elemento guardado con éxito.!", "Continuar");
+            await Navigation.PushAsync(new Resumen(nombre, total));
+
+
+        }
+    }
+}
